7ad1add8df7137c9af4051e7d9c2ebb43994fb08
Dylan Watson <watson93@gmail.com>
2018-05-10 19:56:25 -0400

    Added logic for adding burnt calories

--- routes.js
+++ routes.js
@@ -53,7 +53,7 @@ routes.route('/update/:id').post(function (req, res, next) {
       console.log("User found")
       user.name = req.body.name
       user.calorieGoal = req.body.calorieGoal
-      user.finished = req.body.calorieGoal == 0
+      user.finished = req.body.calorieGoal === 0
       console.log(user)
 
       user.$__save({},
@@ -70,5 +70,43 @@ routes.route('/update/:id').post(function (req, res, next) {
     }
   })
 })
+
+routes.route('/addBurntCalories/:id').post(function (req, res, next) {
+  var id = req.params.id
+  User.findById(id, function (error, user) {
+    if (error) {
+      console.log("User not found");
+      return next(new Error('User was not found'))
+    } else {
+      console.log("User found")
+      user.calorieGoal = calculateCaloriesLeft(user.calorieGoal, req.body.burntCalories)
+      user.finished = user.calorieGoal === 0
+      console.log(user)
+
+      user.$__save({},
+        function (error, user)
+        {
+          if (error) {
+            console.log("ERROR")
+            res.status(400).send('Unable to update user')
+          } else {
+            console.log("NO ERROR")
+            res.status(200).json(user)
+          }
+        })
+    }
+  })
+})
+
+function calculateCaloriesLeft(calorieGoal, burntCalories){
+  console.log(calorieGoal)
+  console.log(burntCalories)
+  console.log("Calculating")
+  calorieGoal = calorieGoal - burntCalories
+  if(calorieGoal < 0)
+    return 0
+  console.log(calorieGoal)
+  return calorieGoal
+}
 
 module.exports = routes
